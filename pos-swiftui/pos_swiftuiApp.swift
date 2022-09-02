0c752dd245d9ecb30e5fc54ac6ad6ba49b861d97
Elder Santos <elder004@gmail.com>
2022-09-02 00:56:46 -0300

    Initial Commit

---
+++ pos-swiftui/pos_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  pos_swiftuiApp.swift
+//  pos-swiftui
+//
+//  Created by Elder Brenner Andrade Santos on 02/09/22.
+//
+
+import SwiftUI
+
+@main
+struct pos_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
